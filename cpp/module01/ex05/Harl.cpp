ac3959983a439d517b1b824e6a257bed007aea01
vrolinno <olivier_nault@hotmail.com>
2024-06-27 16:29:28 -0400

    yeah

--- cpp/module01/ex05/Harl.cpp
+++ cpp/module01/ex05/Harl.cpp
@@ -2,33 +2,33 @@
 
 void Harl::debug( void )
 {
-    cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
 }
 
 void Harl::info( void )
 {
-    cout << "I cannot believe adding extra bacon costs more money. You didn’t put"
-    "enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put"
+    "enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
 }
 void Harl::warning( void )
 {
-    cout <<  "I think I deserve to have some extra bacon for free. I’ve been coming"
-    "for years whereas you started working here since last month." << endl;
+    std::cout <<  "I think I deserve to have some extra bacon for free. I’ve been coming"
+    "for years whereas you started working here since last month." << std::endl;
 }
 void Harl::error( void )
 {
-    cout <<  "This is unacceptable ! I want to speak to the manager now." << endl;
+    std::cout <<  "This is unacceptable ! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain( std::string level )
 {
     int i = 0;
     void (Harl::*Ptrs[])() = {debug, info, warning, error};
-    string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
     while (i != 4 && levels[i].compare(level))
         i++;
     if (i == 4)
-        cout << "wrong levels";
+        std::cout << "wrong levels";
     else
         (this->*Ptrs[i])();
 
